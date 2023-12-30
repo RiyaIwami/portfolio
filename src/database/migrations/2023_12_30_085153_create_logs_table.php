6945d9cf2de6894df2b0f023b4ad4210d41d9a27
RiyaIwami <iwmry22360679@outlook.jp>
2023-12-30 18:08:28 +0900

    Eloquent Model

--- src/database/migrations/2023_12_30_085153_create_logs_table.php
+++ src/database/migrations/2023_12_30_085153_create_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppTables extends Migration
+class CreateLogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,6 @@ class CreateAppTables extends Migration
      */
     public function up()
     {
-        Schema::create('visit_statuses', function (Blueprint $table) {
-            $table->id();
-            // ここにカラムを追加していく
-            $table->timestamps();
-        });
-
-        Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            // ここにカラムを追加していく
-            $table->timestamps();
-        });
-
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
             // ここにカラムを追加していく
@@ -43,8 +31,6 @@ class CreateAppTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visit_statuses');
-        Schema::dropIfExists('categories');
         Schema::dropIfExists('logs');
     }
 }
