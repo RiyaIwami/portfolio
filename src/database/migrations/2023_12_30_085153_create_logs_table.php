30a3d07f2a57c65a9c769bcc9310886ccf7e1524
RiyaIwami <iwmry22360679@outlook.jp>
2024-01-11 16:21:34 +0900

    カラム追加

--- src/database/migrations/2023_12_30_085153_create_logs_table.php
+++ src/database/migrations/2023_12_30_085153_create_logs_table.php
@@ -16,11 +16,18 @@ class CreateLogsTable extends Migration
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
             // ここにカラムを追加していく
+
+            $table->string('name');
+            $table->unsignedBigInteger('category');
+            $table->unsignedBigInteger('visit_status');
+            $table->decimal('score', 2, 1);
+            $table->text('impression');
+            // $table->画像　空欄〇
             $table->timestamps();
 
-            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreignId('visit_status_id')->constrained('visit_statuses')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained('categories')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('visit_status_id')->references('id')->on('visit_statuses');
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
