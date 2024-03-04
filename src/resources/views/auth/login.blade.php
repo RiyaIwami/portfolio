7e400b9030d5c8852b1396ca6c32426cfed1abe6
RiyaIwami <iwmry22360679@outlook.jp>
2024-03-04 14:03:34 +0900

    $errors　追加

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -17,13 +17,13 @@
 
                             <div style="margin-bottom: 5px;"></div>
 
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                            @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                            @if ($errors->has('email'))
+                                <div class="invalid-feedback" role="alert">
+                                    {{ $errors->first('email') }}
+                                </div>
+                            @endif
                         </div>
 
                         <div style="margin-bottom: 20px;"></div>
@@ -33,13 +33,13 @@
 
                             <div style="margin-bottom: 5px;"></div>
 
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="current-password">
 
-                            @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                            @if ($errors->has('password'))
+                                <div class="invalid-feedback" role="alert">
+                                    {{ $errors->first('password') }}
+                                </div>
+                            @endif
                         </div>
 
                         <div style="margin-bottom: 5px;"></div>
