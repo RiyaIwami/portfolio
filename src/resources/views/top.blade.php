b3e8f9a70587ea8fd27f4dbdbaff82ebf061d21b
RiyaIwami <iwmry22360679@outlook.jp>
2024-04-14 02:40:51 +0900

    トップ画面　変更

--- src/resources/views/top.blade.php
+++ src/resources/views/top.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'トップ')
 
 @section('content')
-    <div style="background: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), url('{{ asset('top.avif') }}') no-repeat center center; background-size: cover; height: 95vh; margin: 0; padding: 0;"> 
+    <div style="background: linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.3)), url('{{ asset('top.avif') }}') no-repeat center center; background-size: cover; height: 100vh; margin: 0; padding: 0;"> 
     </div>
 @endsection
 
